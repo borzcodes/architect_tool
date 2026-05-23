f6cfd9d09f3aae28b8631d24fbeab5b02ef0f1e2
borzcodes <realmahmoudcodes@gmail.com>
2026-05-23 03:29:10 +0100

    'Initializing auth with puter'

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from './+types/root';
 import "./app.css";
+import {useEffect, useState} from "react";
+import {getCurrentUser, signIn as puterSignIn, signOut as puterSignOut} from "../lib/puter.action";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -41,7 +43,46 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+const DEFAULT_AUTH_STATE: AuthState = {
+  isSignedIn: false,
+  userName:null,
+  userId:null,
+}
+
 export default function App() {
+  const [authState, setAuthState] = useState<AuthState>(DEFAULT_AUTH_STATE);
+
+  const refreshAuth = async () => {
+    try {
+      const user = await getCurrentUser();
+
+      setAuthState({
+        isSignedIn: !!user,
+        userName: user?.username || null,
+        userId: user?.uuid || null,
+      });
+
+      return !!user;
+    }catch{
+      setAuthState(DEFAULT_AUTH_STATE);
+      return false;
+    }
+  }
+
+  useEffect(()=>{
+    refreshAuth()
+  },[]);
+
+  const signIn = async () => {
+    await puterSignIn();
+    return await refreshAuth();
+  }
+
+  const signOut = async () => {
+    puterSignOut();
+    return await refreshAuth();
+  }
+
   return <Outlet />;
 }
 
