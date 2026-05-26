3565c2a84b868f9493c8f642cda5730dc06ce5e2
borzcodes <realmahmoudcodes@gmail.com>
2026-05-26 16:34:42 +0100

    'Finalizing home section'

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
-import {ArrowRight} from "lucide-react";
+import {ArrowRight, ArrowUpRight, Clock, Layers} from "lucide-react";
 import Button from "../../components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
@@ -40,7 +40,65 @@ export default function Home() {
                       Watch Demo
                   </Button>
               </div>
+
+              <div id='upload' className='upload-shell'>
+                <div className='grid-overlay'/>
+
+                  <div className='upload-card'>
+                      <div className='upload-head'>
+                          <div className='upload-icon'>
+                              <Layers className='icon' />
+                          </div>
+
+                          <h3>Upload your floor plan</h3>
+                          <p>Supports JPG, PNG, formats up to 10MB</p>
+                      </div>
+
+                      <p>Upload images</p>
+                  </div>
+              </div>
           </section>
+
+          <section className='projects'>
+              <div className='section-inner'>
+                  <div className='section-head'>
+                      <div className='copy'>
+                          <h2>Projects</h2>
+                          <p>Your latest work and shared community projects, all in one place.</p>
+                      </div>
+                  </div>
+
+                  <div className='projects-grid'>
+                    <div className='project-card group'>
+                        <div className='preview'>
+                            <img src='https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png' alt='project' />
+
+                            <div className='badge'>
+                                <span>Community</span>
+                            </div>
+                        </div>
+
+                        <div className='card-body'>
+                            <div>
+                                <h3>Project Tanger</h3>
+
+                                <div className='meta'>
+                                    <Clock size={12} />
+                                    <span>{new Date('01.01.2027').toLocaleDateString()}</span>
+                                    <span>By Borz</span>
+                                </div>
+                            </div>
+
+                            <div className='arrow'>
+                                <ArrowUpRight size={18} />
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+              </div>
+          </section>
+
+
       </div>
   )
 }
