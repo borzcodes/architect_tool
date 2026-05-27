0da96f6307b3c18a96680199c2eb2bbf2391317e
borzcodes <realmahmoudcodes@gmail.com>
2026-05-27 22:14:04 +0100

    'Initializing Upload'

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {useOutletContext} from "react-router";
+import {UploadIcon} from "lucide-react";
 
 const Upload = () => {
     const [file, setFile] = useState<File | null> (null);
@@ -10,7 +11,29 @@ const Upload = () => {
 
     return (
         <div className='upload'>
+            {!file ? (
+                <div className={`dropzone ${isDragging ? 'dragging' : ''}`}>
+                    <input type='file' className='drop-input' accept='.jpg, .jpeg, .png' disabled={!isSignedIn}/>
+                    <div className='drop-content'>
+                        <div className='drop-icon'>
+                            <UploadIcon size={20} />
+                        </div>
+                        <p>
+                            {isSignedIn ? (
+                                'Click to upload or just drag here'
+                            ):(
+                                'Sign in or sign up with puter to upload'
+                            )}
+                        </p>
 
+                        <p className='help'>
+                            Maximum file size 50MB.
+                        </p>
+                    </div>
+                </div>
+            ):(
+                <div></div>
+            )}
         </div>
     )
 }
