64054307b58125d07268b299f66e18c99e29c1ef
borzcodes <realmahmoudcodes@gmail.com>
2026-06-06 23:59:52 +0100

    'Implementing just a couple of fixes'

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -14,10 +14,7 @@ export function meta({}: Route.MetaArgs) {
     ];
 }
 
-export default function Home() {
-    const navigate = useNavigate();
-    const [projects, setProjects] = useState<DesignItem[]>([]);
-    const isCreatingProjectRef = useRef(false);
+
 
     const handleUploadComplete = async (base64Image: string) => {
         try {
